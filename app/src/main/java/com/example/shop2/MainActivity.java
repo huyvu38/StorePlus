d42f3cff5260fb12ba2c434eebf31be6fb2aa06f
huyvu38 <vu38@purdue.edu>
2024-06-06 14:55:30 +0700

    Update authentication #1

--- app/src/main/java/com/example/shop2/MainActivity.java
+++ app/src/main/java/com/example/shop2/MainActivity.java
@@ -2,20 +2,32 @@ package com.example.shop2;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.method.PasswordTransformationMethod;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "EmailPassword";
+
+    private FirebaseAuth authentication;
     EditText editEmail1, editPassword1;
     Button loginButton;
     TextView signUpTextView;
@@ -25,12 +37,19 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         //Anh xa
-        //editEmail1 = findViewById(R.id.editEmail1);
-        //editPassword1 = findViewById(R.id.editPassword1);
+        authentication = FirebaseAuth.getInstance();
+        editEmail1 = findViewById(R.id.editEmail1);
+        editPassword1 = findViewById(R.id.editPassword1);
         loginButton = findViewById(R.id.loginButton);
         signUpTextView = findViewById(R.id.signUpTextView);
 
         //Xu ly
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn(editEmail1.getText().toString(), editPassword1.getText().toString());
+            }
+        });
         signUpTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -45,4 +64,31 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
     }
+    private void signIn(String email, String password) {
+        // [START sign_in_with_email]
+        authentication.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = authentication.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                    }
+                });
+        // [END sign_in_with_email]
+    }
+    private void updateUI(FirebaseUser user) {
+
+    }
+
+
 }
--- app/src/main/java/com/example/shop2/Register.java
+++ app/src/main/java/com/example/shop2/Register.java
@@ -1,22 +1,43 @@
 package com.example.shop2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Register extends AppCompatActivity {
-    EditText editEmail2, editPassword2, editAge, editPhone, editHomeAddress;
-    RadioButton radioButtonMale, radioButtonFemale, radioButtonOther;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.HashMap;
+
+public class Register extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private static final String TAG = "EmailPassword";
+
+    EditText editEmail2, editPassword2;
+    //editAge, editPhone, editHomeAddress;
+    //RadioButton radioButtonMale, radioButtonFemale, radioButtonOther;
     Button registerButton;
+    //FirebaseDatabase database;
+    //DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,36 +46,114 @@ public class Register extends AppCompatActivity {
         setContentView(R.layout.activity_register);
 
         //Anh xa
-        //editEmail1 = findViewById(R.id.editEmail1);
-        //editPassword1 = findViewById(R.id.editPassword1);
-        radioButtonMale = findViewById(R.id.radioButtonMale);
-        radioButtonFemale = findViewById(R.id.radioButtonFemale);
-        radioButtonOther = findViewById(R.id.radioButtonOther);
+        //database = FirebaseDatabase.getInstance();
+        //reference = database.getReference();
+        editEmail2 = findViewById(R.id.editEmail2);
+        editPassword2 = findViewById(R.id.editPassword2);
+        //editAge = findViewById(R.id.editAge);
+        //editPhone = findViewById(R.id.editPhone);
+        //editHomeAddress = findViewById(R.id.editHomeAddress);
+        //radioButtonMale = findViewById(R.id.radioButtonMale);
+        //radioButtonFemale = findViewById(R.id.radioButtonFemale);
+        //radioButtonOther = findViewById(R.id.radioButtonOther);
+        registerButton = findViewById(R.id.registerButton); // Initialize registerButton
+
+        // Register Button Click Listener
         /*
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                HashMap<String, Object> information = new HashMap<>();
+                String email = editEmail2.getText().toString();
+                String password = editPassword2.getText().toString();
+                int age = Integer.parseInt(editAge.getText().toString());
+                String gender = "";
                 if (radioButtonMale.isChecked()) {
-
+                    gender = "Male";
                 }
                 if (radioButtonFemale.isChecked()) {
-
+                    gender = "Female";
                 }
                 if (radioButtonOther.isChecked()) {
-
+                    gender = "Other";
                 }
+                int phone = Integer.parseInt(editPhone.getText().toString());
+                String homeAddress = editHomeAddress.getText().toString();
+                //After create account successfully
+                information.put("Email", email);
+                information.put("Password", password);
+                information.put("Age", age);
+                information.put("Gender", gender);
+                information.put("Phone", phone);
+                information.put("Address", homeAddress);
+                reference.child("User")
+                        .setValue(information)
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+
+                    public void onSuccess(Void unused) {
+                        Toast.makeText(Register.this,
+                                "Create account successfully",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(Register.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
             }
         });
 
          */
-
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createAccount(editEmail2.getText().toString(), editPassword2.getText().toString());
+                finish();
+            }
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
-    public void onRadioButtonClicked(View view) {
+    private void createAccount(String email, String password) {
+        // [START create_user_with_email]
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(Register.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                    }
+                });
+        // [END create_user_with_email]
+    }
+    /*
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            reload();
+        }
+    }
+
+     */
+    private void reload() { }
+    private void updateUI(FirebaseUser user) {
 
     }
 }
